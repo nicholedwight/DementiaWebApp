841e0634e7fec46089a34e201bf14264281a2259
Nichole Dwight <nicholedwight@gmail.com>
2015-03-09 07:01:18 +0000

    added nav content, working on styling nav

--- inc/header.php
+++ inc/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>DementiaWeb</title>
     <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/styles.css" />
     <script src="js/vendor/modernizr.js"></script>
   </head>
   <body>
@@ -12,7 +13,7 @@
       <ul class="title-area">
         <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
         <li class="name">
-          <h1><a href="#">My Site</a></h1>
+          <h1><a href="index.php">DementiaWeb</a></h1>
         </li>
 
       </ul>
@@ -20,14 +21,43 @@
       <section class="top-bar-section">
         <!-- Right Nav Section -->
         <ul class="right">
-          <li class="active"><a href="#">Right Button Active</a></li>
+          <li><a href="#">Worried about your memory?</a></li>
           <li class="has-dropdown">
-            <a href="#">Right Button Dropdown</a>
+            <a href="#">Benefits and Legal</a>
             <ul class="dropdown">
-              <li><a href="#">First link in dropdown</a></li>
-              <li class="active"><a href="#">Active link in dropdown</a></li>
+              <li><a href="#">Benefits</a></li>
+              <li><a href="#">Power of Attorney</a></li>
+              <li><a href="#">Local Council</a></li>
             </ul>
           </li>
+          <li class="has-dropdown">
+            <a href="#">Staying Active</a>
+            <ul class="dropdown">
+              <li><a href="#">Group Forum</a></li>
+              <li><a href="#">Support Groups</a></li>
+              <li><a href="#">Activities</a></li>
+              <li><a href="#">Organisations</a></li>
+              <li><a href="#">In the Home</a></li>
+            </ul>
+          </li>
+          <li class="has-dropdown">
+            <a href="#">Carer Health</a>
+            <ul class="dropdown">
+              <li><a href="#">Carer's Support Groups</a></li>
+              <li><a href="#">Carer Training</a></li>
+            </ul>
+          </li>
+          <li class="has-dropdown">
+            <a href="#">Your Health</a>
+            <ul class="dropdown">
+              <li><a href="#">What to Expect</a></li>
+              <li><a href="#">Specialist Health Physicians</a></li>
+            </ul>
+          </li>
+          <li><strong>Keep in touch</strong></li>
+          <li class="li-indented"><a href="#">Contact Us</a></li>
+          <li class="li-indented"><a href="#">Helpline</a></li>
         </ul>
+
       </section>
     </nav>

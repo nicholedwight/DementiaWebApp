c85ce9fa51892df1c9f67a378776598ee3a01829
Nichole Dwight <nicholedwight@gmail.com>
2015-03-16 15:59:06 +0000

    nav toggle actually works now

--- inc/header.php
+++ inc/header.php
@@ -9,65 +9,53 @@
     <script src="js/vendor/modernizr.js"></script>
   </head>
   <body>
-    <nav class="top-bar" data-topbar role="navigation">
-      <ul>
-        <li class="toggle-topbar">
-          <a href="#" id="nav-toggle">
+    <header>
+        <div class="navbar-header">
+          <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse" id="nav-toggle">
             <span></span>
-            <div class="menu-toggle">Menu</div>
           </a>
-        </li>
-        <li class="name">
-          <h1><a href="index.php">Dementia Web</a></h1>
-        </li>
-        <li>
-          <a href="#">Search</a>
-        </li>
-      </ul>
-
-      <section class="top-bar-section">
-        <!-- Right Nav Section -->
-        <ul>
-          <li><a href="index.php">Home</a></li>
-          <hr>
-          <li><a href="#">Worried about your memory?</a></li>
-          <li class="has-dropdown">
-            <a href="#">Benefits and Legal</a>
-            <ul class="dropdown">
-              <li><a href="#">Benefits</a></li>
-              <li><a href="#">Power of Attorney</a></li>
-              <li><a href="#">Local Council</a></li>
-            </ul>
-          </li>
-          <li class="has-dropdown">
-            <a href="#">Staying Active</a>
-            <ul class="dropdown">
-              <li><a href="#">Group Forum</a></li>
-              <li><a href="#">Support Groups</a></li>
-              <li><a href="#">Activities</a></li>
-              <li><a href="#">Organisations</a></li>
-              <li><a href="#">In the Home</a></li>
-            </ul>
-          </li>
-          <li class="has-dropdown">
-            <a href="#">Carer Health</a>
-            <ul class="dropdown">
-              <li><a href="#">Carer's Support Groups</a></li>
-              <li><a href="#">Carer Training</a></li>
-            </ul>
-          </li>
-          <li class="has-dropdown">
-            <a href="#">Your Health</a>
-            <ul class="dropdown">
-              <li><a href="#">What to Expect</a></li>
-              <li><a href="#">Specialist Health Physicians</a></li>
-            </ul>
-          </li>
-          <hr>
-          <li><strong>Keep in touch</strong></li>
-          <li class="li-indented"><a href="#">Contact Us</a></li>
-          <li class="li-indented"><a href="#">Helpline</a></li>
-        </ul>
-
-      </section>
-    </nav>
+          <a class="navbar-brand" href="index.php">DementiaWeb</a>
+        </div>
+        <nav class="navbar-collapsible collapsed">
+          <ul class="nav navbar-nav">
+      		    <li class="active"><a href="index.php">Home</a></li>
+              <li><a href="#">Worried about your memory?</a></li>
+              <li class="has-dropdown">
+                <a href="#">Benefits and Legal</a>
+                <ul class="dropdown collapsed">
+                  <li><a href="#">Benefits</a></li>
+                  <li><a href="#">Power of Attorney</a></li>
+                  <li><a href="#">Local Council</a></li>
+                </ul>
+              </li>
+              <li class="has-dropdown">
+                <a href="#">Staying Active</a>
+                <ul class="dropdown collapsed">
+                  <li><a href="#">Group Forum</a></li>
+                  <li><a href="#">Support Groups</a></li>
+                  <li><a href="#">Activities</a></li>
+                  <li><a href="#">Organisations</a></li>
+                  <li><a href="#">In the Home</a></li>
+                </ul>
+              </li>
+              <li class="has-dropdown">
+                <a href="#">Carer Health</a>
+                <ul class="dropdown collapsed">
+                  <li><a href="#">Carer's Support Groups</a></li>
+                  <li><a href="#">Carer Training</a></li>
+                </ul>
+              </li>
+              <li class="has-dropdown">
+                <a href="#">Your Health</a>
+                <ul class="dropdown collapsed">
+                  <li><a href="#">What to Expect</a></li>
+                  <li><a href="#">Specialist Health Physicians</a></li>
+                </ul>
+              </li>
+              <hr>
+              <li><strong>Keep in touch</strong></li>
+              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">Helpline</a></li>
+      		</ul>
+        </nav>
+    </header>
--- js/global.js
+++ js/global.js
@@ -1,4 +1,29 @@
-document.querySelector( "#nav-toggle" )
-  .addEventListener( "click", function() {
-    this.classList.toggle( "active" );
-  });
+// document.querySelector( "#nav-toggle" )
+//   .addEventListener( "click", function() {
+//     this.classList.toggle( "active" );
+//   });
+//
+//   $(function () {
+//     $('.click-nav > ul').toggleClass('no-js js');
+//     $('.click-nav .js ul').hide();
+//     $('.click-nav .js').click(function(e) {
+//       $('.click-nav .js ul').slideToggle(200);
+//       $('.clicker').toggleClass('active');
+//       e.stopPropagation();
+//     });
+//     $(document).click(function() {
+//       if ($('.click-nav .js ul').is(':visible')) {
+//         $('.click-nav .js ul', this).slideUp();
+//         $('.clicker').removeClass('active');
+//       }
+//     });
+//   });
+
+  $(document).ready(function () {
+  			  $(".navbar-toggle").on("click", function () {
+  				    $(".navbar-collapsible").toggleClass("collapsed");
+  			  });
+          $(".has-dropdown").on("click", function () {
+  				    $(this).find(".dropdown").toggleClass("collapsed");
+  			  });
+  		});
